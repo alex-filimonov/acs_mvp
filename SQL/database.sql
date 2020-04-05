2762bc8521dd1033d2a0d4f7828e1422a206e085
leo <filimonov_leha@bk.ru>
2020-04-05 23:42:49 +1000

    add sql

--- SQL/database.sql
+++ SQL/database.sql
@@ -11,8 +11,31 @@ create table cpe (
     vendor_name varchar(255),
     model_name varchar(255),
     all_inform text,
-    first_inform TIMESTAMP NOT NULL,
-    last_inform TIMESTAMP NOT NULL
+    first_inform TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    last_inform TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+create table commands (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    cpe_id integer,
+    command integer,
+    request text,
+    response text,
+    status_id integer,
+    create_time  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    update_time  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+create table commands_type (
+    id INTEGER NOT NULL,
+    command_name varchar(255)
+);
+
+
+
+delete from commands_type;
+insert into commands_type (id,command_name) values (1,'GetParametrName');
+insert into commands_type (id,command_name) values (2,'GetParametrValue');
+
 
 
